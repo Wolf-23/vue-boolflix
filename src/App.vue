4906e30124e861075e44fe4b897f083f71e4806b
Riccardo <lupinu.ric@gmail.com>
2022-08-30 18:14:23 +0200

    Project: Mileston 1 Complete

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MyHeader @searchText="getSearchText"/>
+    <MyMain :listFilm="listFilm"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MyHeader from './components/MyHeader.vue'
+import MyMain from './components/MyMain.vue';
+import axios from 'axios';
 export default {
   name: 'App',
+  data() {
+    return {
+      MySearchText: '',
+      listFilm: []
+    }
+  },
   components: {
-    HelloWorld
+    MyHeader,
+    MyMain
+  },
+  methods: {
+    getSearchText(data) {
+      this.MySearchText = data;
+      this.apiRequest();
+    },
+    apiRequest() {
+                axios.get(`https://api.themoviedb.org/3/search/movie?api_key=c4a0217aed11ceee398209d64761d218&query=${this.MySearchText}&language=it-IT`)
+                .then(response => {
+                    this.listFilm = response.data.results;
+                    console.log(this.listFilm)
+                })
+            }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+      <input type="text" placeholder="Titolo Film o SerieTv:" v-model="searchText">
+      <button @click="$emit('searchText', searchText)">Cerca</button>
+    </div>
+  </template>
+  
+  <script>
+
+  export default {
+    name: 'MyHeader',
+    data() {
+      return {
+        searchText: '',
+      }
+    },
+    
+  }
+  </script>
+  
+  <style lang="scss">
+  
+  </style>
+  
