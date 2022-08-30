df7f7df5e85805a30f8d44dd554a2d3748200597
Riccardo <lupinu.ric@gmail.com>
2022-08-30 18:13:29 +0200

    Project: Mileston 1 Complete

---
+++ src/components/MyMain.vue
@@ -0,0 +1,38 @@
+<template>
+  <main>
+        <ul v-for="(film, index) in listFilm" :key="index">
+            <li class="title">{{film.title}}</li>
+            <li>{{film.original_title}}</li>
+            <li>{{film.original_language}}</li>
+            <li>{{film.vote_average}}</li>
+        </ul>
+  </main>
+</template>
+
+<script>
+  
+    export default {
+        name: 'MyMain',
+        props: {
+            listFilm: Array
+        },
+        data() {
+            return{
+               
+            }
+        },
+        methods: {
+            
+        },
+    }
+</script>
+
+<style scoped lang="scss">
+    ul {
+        border: 1px solid black;
+        .title {
+            font-size: 20px;
+            font-weight: bold;
+        }
+    }
+</style>
