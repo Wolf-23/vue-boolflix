09a00bd092dbaf3bf6091c26e7db9358c0e8fc70
Riccardo <lupinu.ric@gmail.com>
2022-08-31 15:50:10 +0200

    Project: MileStone 2 done

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -1,19 +1,25 @@
 <template>
   <main>
+    <div>
       <h3 v-if="visibility == true && this.listFilm.lenght > 0">Lista Film Cercati:</h3>
       <ul v-for="(film, index) in listFilm" :key="index">
             <li class="title">{{film.title}}</li>
             <li>{{film.original_title}}</li>
-            <li>{{film.original_language}}</li>
+            <li>
+                <img :src="getImage(film.original_language)" :alt="film.original_language">
+            </li>
             <li>{{film.vote_average}}</li>
         </ul>
+    </div>
+    <div>
         <h3 v-if="visibility == true && this.listFilm.lenght > 0">Lista Serie Cercate:</h3>
         <ul v-for="(serie, index) in listSerie" :key="index">
             <li class="title">{{serie.name}}</li>
             <li>{{serie.original_name}}</li>
-            <li>{{serie.original_language}}</li>
+            <li><img :src="getImage(serie.original_language)" :alt="serie.original_language"></li>
             <li>{{serie.vote_average}}</li>
         </ul>
+    </div>
   </main>
 </template>
 
@@ -26,7 +32,22 @@
             listSerie: Array,
             visibility: Boolean
 
-        }
+        },
+        data() {
+            return {
+            }
+        },
+        methods: {
+            getImage(language) {
+                let imageSrc = '';
+                if(language == 'en' || language == 'it' || language == 'fr') {
+                    imageSrc = require('../assets/flag/flag-of-' + language + '.png');
+                } else {
+                    imageSrc = language;
+                }
+                return imageSrc;
+            }
+        },
     }
 </script>
 
@@ -36,6 +57,9 @@
         .title {
             font-size: 20px;
             font-weight: bold;
+        }
+        img {
+            height: 20px;
         }
     }
 </style>
